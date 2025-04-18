ffeef7ae92b033ef81c205eb12e7f8394df42e76
Sean <seanpmurdoch@proton.me>
2025-04-18 13:04:34 +0100

    Logic for grep functionality
    Reads the file passed as an argument and if pattern in file prints the line

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
+    /// Pattern to search for
     pattern: String,
+    /// Path to the file to read
     path: std::path::PathBuf,
 }
-    
-fn main() {
-   let args = Cli::parse();
 
-   println!("pattern: {:?}, path: {:?}", args.pattern, args.path); 
+fn main() {
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
