315a7a80357bf8a5015682541148ae45e1cfbc72
Sean <seanpmurdoch@proton.me>
2025-04-18 11:34:07 +0100

    switch to using clap crate
    thord party library will handle parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-struct cli {
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
-
-fn main() {
-    let pattern = std::env::args()
-        .nth(1)
-        .expect("No pattern given");
-    let path = std::env::args()
-        .nth(2)
-        .expect("No path given");
-
-    println!("pattern: {:?}, path: {:?}", pattern, path);
-}
     
+fn main() {
+   let args = Cli::parse();
+
+   println!("pattern: {:?}, path: {:?}", args.pattern, args.path); 
+}
